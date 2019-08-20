ffd7c935cba653df6a9e2c65b4025de78523c078
MrT3313 <rturgeon13@gmail.com>
2019-08-20 12:41:10 -0500

    closure.js completed

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,33 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const foo = 'bar';
+function returnFoo() {
+  return foo;
+}
+console.log(foo)
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+
+// Good Explanation: https://medium.com/dailyjs/i-never-understood-javascript-closures-9663703368e8
+
+function createCounter() {
+  let counter = 0
+  // - V1 - //
+    // const myFunction = function() {
+  // - V2 - //
+    const myFunction = () => {
+
+    counter = counter + 1
+    return counter
+  }
+  return myFunction
+}
+const increment = createCounter()
+const c1 = increment()
+const c2 = increment()
+const c3 = increment()
+console.log('example increment', c1, c2, c3)
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
